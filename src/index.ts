a665515c01fca1b69c735c2600e57cc73bc729be
JoeBlack220 <348653205@qq.com>
2020-08-02 18:56:14 -0500

    interact with definer

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,17 @@ import { logger } from './lib/logger';
 import { GasPriceExecutor } from './lib/GasPriceExecutor';
 import './lib/env';
 import { web3 } from './proxy/web3';
+import { getInstance } from './lib/getContractInstance'
 // let gasPriceExecutor = new GasPriceExecutor()
 // gasPriceExecutor.start();
-web3.eth.getAccounts().then((acc) => {
-    console.log(acc);
-});
+// web3.eth.getAccounts().then((acc) => {
+//     console.log(acc);
+// });
 
-console.log();
+(async () => {
+    const savingAccount = await getInstance("SavingAccount", web3, "0x772a1211551D431fa9aC12D56640163495309F5d");
+    const blockNum = savingAccount.methods.getBlockNumber().call();
+    return blockNum;
+})().then(e => console.log(e))
+
+
--- src/proxy/web3.ts
+++ src/proxy/web3.ts
@@ -1,3 +1,3 @@
 import Web3 from 'web3';
+export const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
-export let web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
